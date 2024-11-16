38644675f1112e81c6bd8c0f6f6f7df2bf7a543f
Edson Correia <edsoncorreia997@gmail.com>
2024-11-16 20:26:52 -0300

    criando e estilizando pagina detalhes

--- estabulo/src/app/pages/detalhes-cavalo/detalhes-cavalo.component.ts
+++ estabulo/src/app/pages/detalhes-cavalo/detalhes-cavalo.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-detalhes-cavalo',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './detalhes-cavalo.component.html',
   styleUrl: './detalhes-cavalo.component.scss'
 })
